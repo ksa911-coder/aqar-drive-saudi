77a2d1b6cc90c60832a9ecef6e6baf7583a3e9b7
Abu Thamer <ksa91180@hotmail.com>
2026-09-24 15:41:38 +0300

    fix: add tailwindcss vite plugin for tailwind v4

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     TanStackRouterVite(),
     tsconfigPaths(),
