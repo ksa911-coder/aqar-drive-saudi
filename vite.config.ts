deda2c35f88eaf49a274a9956fe348ce8f43f95c
Abu Thamer <ksa91180@hotmail.com>
2026-09-24 15:11:24 +0300

    fix: add TanStackRouterVite plugin to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    tsconfigPaths(),
+  ],
 })
